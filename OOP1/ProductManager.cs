e11fa61774ff0d30d2f695e93c8bafbf5c8347c4
Cihan Sevim <earjsd@hotmail.com>
2021-04-08 10:27:39 +0300

    Yazılım Geliştirici Yetiştirme Kampı - 3. Ders Sonu Ödevi

--- OOP1/ProductManager.cs
+++ OOP1/ProductManager.cs
@@ -6,15 +6,15 @@ namespace OOP1
 {
     public class ProductManager
     {
-        public void Add(Product product)
-        {
-            Console.WriteLine(product.ProductName + " eklendi.");
-        }
+            public void Add(Product product)
+            {
+                Console.WriteLine(product.ProductName + " eklendi.");
+            }
 
-        public void Update(Product product)
-        {
-            Console.WriteLine(product.ProductName + " güncellendi.");
-        }
+            public void Update(Product product)
+            {
+                Console.WriteLine(product.ProductName + " güncellendi.");
+            }
 
     }
 }
